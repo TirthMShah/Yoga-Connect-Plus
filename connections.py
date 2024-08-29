74344871323ecce1087ff9feaad3f888465406d9
TirthMShah <116023656+TirthMShah@users.noreply.github.com>
2024-08-30 02:40:34 +0530

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,6 +1,6 @@
 import psycopg2
 
 def connection():
-    conn=psycopg2.connect(host="localhost",port="5432",user="postgres",password="hetu",database="YogaConnect+")
+    conn=psycopg2.connect(host="yogaconnect_user",port="5432",user="dpg-cr8cm2q3esus73b4sil0-a.oregon-postgres.render.co",password="ms2WdMIdUJ9fCh1JsMvXBOK6O93gozYn",database="yogaconnect")
     cur=conn.cursor()
     return conn,cur
