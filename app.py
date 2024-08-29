296b27a4246282351dc85257e56d3ea730b26540
TirthMShah <116023656+TirthMShah@users.noreply.github.com>
2024-08-30 02:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ from flask import Flask, render_template, request, redirect, url_for,Response,se
 # Create a flask app instance.
 app = Flask(__name__)
 
-app.secret_key = 'hetu' # For OTP rest API purpose.
+app.secret_key = 'tirth' # For OTP rest API purpose.
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://yogaconnect_user:ms2WdMIdUJ9fCh1JsMvXBOK6O93gozYn@dpg-cr8cm2q3esus73b4sil0-a.oregon-postgres.render.com/yogaconnect'
